77d6df09f4e504fd1fd1690886985eb941719066
Nick Margaritondo <86981780+Nickm615@users.noreply.github.com>
2021-10-21 17:49:39 -0500

    Created initial file structure and basic routes.

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const path = require('path');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+
+app.get('/notes',(req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+}),
+
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+}),
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`))
