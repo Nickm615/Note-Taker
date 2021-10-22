89c11b6acbca862e8eacaf5c78e0b2758c192be2
Nick Margaritondo <86981780+Nickm615@users.noreply.github.com>
2021-10-22 09:34:14 -0500

    Fixed button

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const path = require('path');
+const notes = require('./db/db.json')
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
@@ -15,4 +16,11 @@ app.get('/notes',(req, res) => {
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
 }),
+
+app.get('/api/notes', (req, res)=>{
+    console.log(notes)
+    res.status(200).json(notes);
+}),
+
+app.post('/api/notes')
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`))
